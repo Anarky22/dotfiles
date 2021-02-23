6ed6c93ee013028e9c9c5dcd8b48fb86dbfaf66b
Anarky22 <goawaynowplease22@gmail.com>
2021-02-22 21:59:15 -0500

    Add mirrorlist update script

---
+++ mirrorlist_update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Commands to:
+# Backs up the mirrorlist, then pulls the latest list of https mirrors from the arch linux mirrorlist,
+# uncomments all the servers, tests and finds the 10 fastest,
+# and then updates the mirrorlist and if successfull cats the file
+
+# see wiki.archlinux.org/index.php/Mirrors#Sorting_mirrors
+
+# backup
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+# pull and uncomment mirror list, store at /tmp/unsorted_mirrorlist
+curl -s "https://archlinux.org/mirrorlist/all/https/" | sed -e 's/^#Server/Server/' -e '/^#/d' > /tmp/unsorted_mirrorlist
+# Rank top 10 mirrors and store at /tmp/mirrorlist
+rankmirrors -n 10 /tmp/unsorted_mirrorlist > /tmp/mirrorlist
+# see new mirrorlist
+cat /tmp/mirrorlist
+# change pacman mirrorlist
+cp /tmp/mirrorlist /etc/pacman.d/mirrorlist
+
+# delete tmp files
+rm /tmp/unsorted_mirrorlist
+rm /tmp/mirrorlist
